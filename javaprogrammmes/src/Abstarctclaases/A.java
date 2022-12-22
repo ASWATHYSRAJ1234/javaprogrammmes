e7ff8319ba77d4bab640ed44fb9ec308656bb499
Aswathysraj <121033746+Aswathysraj@users.noreply.github.com>
2022-12-22 13:55:19 +0530

    First commit

---
+++ javaprogrammmes/src/Abstarctclaases/A.java
@@ -0,0 +1,11 @@
+package Abstarctclaases;
+
+import java.util.Scanner;
+
+public abstract class A {
+public double workinghours;
+public abstract void display();
+
+
+}
+
---
+++ javaprogrammmes/src/Patternprogram/Aggregationexample1.java
@@ -0,0 +1,12 @@
+package Patternprogram;
+
+public class Aggregationexample1 {
+	int rollnumber;
+	String name;
+	public Aggregationexample1(int rollnuber ,String name)
+	{
+		this.rollnumber=rollnumber;
+		this.name=name;
+	}
+
+}
